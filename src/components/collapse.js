7c97438b7097b9344c87fb4e8a49010a057964d9
lysiandon <don.lysiane@gmail.com>
2023-06-02 13:26:28 +0200

    finished a propos page

--- src/components/collapse.js
+++ src/components/collapse.js
@@ -1,11 +1,14 @@
 import '../styles/collapse.scss';
-import chevron from '../assets/chevron-haut.png'
+import { useState } from 'react';
 export default function Collapse({data}) {
+    const [isCollapseOpen, setCollapse] = useState(false);
     return (
       <>
         <div id="collapse">
-            <div className="header"> <span>{data.header}</span> <img src={chevron} alt='chevron haut'/></div>
-            <div className="content"> {data.content} </div>
+            <div className="header" onClick={() => setCollapse(!isCollapseOpen)}> <span>{data.header}</span> 
+            <div className={`chevron ${isCollapseOpen ? 'chevron-up' : 'chevron-down'}`}></div>
+            </div>
+            <div className={`content ${isCollapseOpen ? "" : "closed"}`}> {isCollapseOpen && data.content} </div>
         </div>
       </>
     );
--- src/pages/aPropos.js
+++ src/pages/aPropos.js
@@ -2,25 +2,8 @@ import Banner from '../components/banner';
 import mountainsImg from '../assets/mountains.png';
 import '../styles/aPropos.scss'
 import Collapse from '../components/collapse';
+import values from '../data/values.json';
 export default function APropos() {
-  const values = [
-    {
-      header: "Fiabilité",
-      content:  "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
-    },
-    {
-      header: "Respect",
-      content: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-    },
-    {
-      header: "Service",
-      content:  "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
-    },
-    { 
-      header: "Sécurité",
-      content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
-    },
-  ]
 
   const listValues = values.map(value => {
     return <div className='value' key={value.header}>
