9b2a121ed2ddc2821375293d7e1f672c3ccb8f19
lysiandon <don.lysiane@gmail.com>
2023-07-23 19:20:30 +0200

    fixed bugs carousel

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,16 +1,37 @@
 import '../styles/carousel.scss';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export default function Carousel({ pictures, altImg }) { 
     
     const [currentCarouselWidth, setCurrentCarouselWidth] = useState(0);
     const [imagesLoaded, setImagesLoaded] = useState(false);
     const [currentPicture, setCurrentPicture] = useState(1);
-    // const [updatedWidth, setUpdatedWidth] = useState(0);
     let updatedWidth = 0;
-    
-    // au resize: scrollleft = 0 + (currentPicture * currentCarouselWith)
-    
+
+    // Handle the window resize:
+    useEffect(() => {
+        const handleResize = () => {
+          let carouselWidth = document.querySelector(".slides-container").offsetWidth;
+          const totalWidth = pictures.length * carouselWidth;
+      
+          // Calculate the current slide position (in pixels) based on the currentPicture
+          const currentSlidePosition = (currentPicture - 1) * carouselWidth;
+      
+          // Check if the new currentSlidePosition is within the valid range
+          if (currentSlidePosition >= 0 && currentSlidePosition <= totalWidth - carouselWidth) {
+            document.querySelector(".slides-container").scrollLeft = currentSlidePosition;
+            setCurrentCarouselWidth(currentSlidePosition);
+          }
+        };
+      
+        window.addEventListener('resize', handleResize);
+      
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+    }, [currentPicture, pictures]);
+      
+      
     function handleImageLoad() {
       setImagesLoaded(true);
       return imagesLoaded;
@@ -31,38 +52,39 @@ export default function Carousel({ pictures, altImg }) {
 
         if (currentCarouselWidth >=  totalWidth - carouselWidth) {
             updatedWidth = 0;
-            document.querySelector(".slides-container").scrollLeft = updatedWidth;
-            setCurrentCarouselWidth(0);
         } else {
-            updatedWidth = currentPicture * currentCarouselWidth;
-            console.log(updatedWidth);
-            console.log("currentPicture", currentPicture);
-            console.log("currentCarouselWidth", currentCarouselWidth);
-            document.querySelector(".slides-container").scrollLeft = updatedWidth;
-            setCurrentCarouselWidth(currentCarouselWidth + carouselWidth);
+            updatedWidth = currentPicture * carouselWidth;
         }
+        document.querySelector(".slides-container").scrollLeft = updatedWidth;
+        setCurrentCarouselWidth(updatedWidth);
     }
+
     function SlideToPrevImg() {
         // Guard
-        if (pictures.length <= 1 ) {
+        if (pictures.length <= 1) {
             return;
         }
+    
         const carouselWidth = document.querySelector(".slides-container").offsetWidth;
         const totalWidth = pictures.length * carouselWidth;
-        if (currentPicture === 1) {
-            setCurrentPicture(pictures.length);
-        } else{
-            setCurrentPicture(currentPicture - 1);
-        }
-
-        if (currentCarouselWidth ===  0) {
-            document.querySelector(".slides-container").scrollLeft = totalWidth - carouselWidth;
-            setCurrentCarouselWidth(totalWidth - carouselWidth);
+    
+        // Calculate the previous picture index
+        let prevPicture = currentPicture === 1 ? pictures.length : currentPicture - 1;
+        let updatedWidth;
+    
+        if (currentCarouselWidth === 0) {
+            // When reaching the first image, we want to show the last image on the left
+            updatedWidth = totalWidth - carouselWidth;
         } else {
-            setCurrentCarouselWidth(currentCarouselWidth - carouselWidth);
-            document.querySelector(".slides-container").scrollLeft -= carouselWidth;
+            // Slide to the previous image normally
+            updatedWidth = (prevPicture - 1) * carouselWidth;
         }
+    
+        setCurrentPicture(prevPicture);
+        document.querySelector(".slides-container").scrollLeft = updatedWidth;
+        setCurrentCarouselWidth(updatedWidth);
     }
+    
     const gallery = pictures.map((p, index) => <img src={p} alt={altImg} className='slide' key={index} onLoad={handleImageLoad}/> )
 
   return <div id="carousel"><section className="slider-wrapper">
